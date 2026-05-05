3b0f5e6968006e8b112806d6d8fd76763e642b0d
omarramy22 <omarramy@aucegypt.edu>
2026-05-05 17:36:10 +0300

    1.5 Exercise

--- part1/course_info/vite-project/src/App.jsx
+++ part1/course_info/vite-project/src/App.jsx
@@ -1,5 +1,5 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
 }
 const Part = (props) => {
   return <div> {props.part} {props.exercises} </div>
@@ -19,8 +19,9 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -34,12 +35,13 @@ const App = () => {
       exercises: 14
     }
   ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
