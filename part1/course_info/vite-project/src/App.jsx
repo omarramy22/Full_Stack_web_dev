b9c42b17be850ee8a7ff4439c794c0b1da0b1ff3
omarramy22 <omarramy@aucegypt.edu>
2026-05-12 17:23:15 +0300

    update for production

--- part1/course_info/vite-project/src/App.jsx
+++ part1/course_info/vite-project/src/App.jsx
@@ -1,63 +1,66 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Filter from './components/search'
-import countriesService from './services/countries'
+import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
-  const [countries, setCountries] = useState([])
-  const [search, setSearch] = useState('')
-  const [weather, setWeather] = useState(null)
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    countriesService.getAllCountries().then(data => {
-      setCountries(data)
+    noteService.getAll().then((data) => {
+      setNotes(data)
     })
   }, [])
 
-  const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
 
-  useEffect(() => {
-    if (search) {
-      if (filteredCountries.length === 1) 
-        countriesService.getWeather(filteredCountries[0].capital).then(data => {
-          setWeather(data)
-        })
+    noteService.update(id, changedNote).then((data) => {
+      setNotes(notes.map((n) => (n.id !== id ? n : data)))
+    })
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
     }
-  }, [filteredCountries])
 
+    noteService.create(noteObject).then((data) => {
+      setNotes(notes.concat(data))
+      setNewNote('')
+    })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
-      <Filter searchTerm={search} setSearchTerm={setSearch} />
-      <h2>Countries</h2>
-      {filteredCountries.length === 0 && <p>No matches found.</p>}
-      {filteredCountries.length === 1 && (
-        <div>
-          <h3>{filteredCountries[0].name.common}</h3>
-          <p>Capital: {filteredCountries[0].capital}</p>
-          <p>Population: {filteredCountries[0].population}</p>
-          <p>Languages: {Object.values(filteredCountries[0].languages).join(', ')}</p>
-          <p>Area: {filteredCountries[0].area}</p>
-          <img src={filteredCountries[0].flags.png} alt={`Flag of ${filteredCountries[0].name.common}`} width="150" />
-        </div>
-      )}
-      {filteredCountries.length < 10 ? (
-        filteredCountries.map(country => (
-          <div key={country.cca3}>
-            {country.name.common} <button onClick={() => setSearch(country.name.common)}>show</button>
-          </div>
-        ))
-      ) : (
-        <p>Too many matches. Please be more specific.</p>
-      )}
-      {filteredCountries.length === 1 && (
-        <div>
-          <h3>Weather in {filteredCountries[0].capital}</h3>
-          <p>Temperature: {weather?.main.temp} °C</p>
-          <p>Wind: {weather?.wind.speed} m/s</p>
-        </div>
-      )}
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
+
 export default App
--- part3/temp.js
+++ part3/temp.js
@@ -1,74 +1,98 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
 ]
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/info', (req, res) => {
+  const date = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
   } else {
     response.status(404).end()
   }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
   response.status(204).end()
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
+  return String(Math.floor(Math.random() * 1000000))
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (!body.name) {
     return response.status(400).json({ 
-      error: 'content missing' 
+      error: 'name missing' 
     })
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
     id: generateId(),
   }
 
-  notes = notes.concat(note)
+  persons = persons.concat(person)
 
-  response.json(note)
+  response.json(person)
 })
 
 const PORT = 3001
