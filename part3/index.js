a1b909aa46b4a712750ccc97db8c1c90a326e09e
omarramy22 <omarramy@aucegypt.edu>
2026-05-13 05:25:40 +0300

    Exercises 3.14 to 3.18

--- part3/index.js
+++ part3/index.js
@@ -3,9 +3,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
+
 
 const Person = require('./models/person')
 
@@ -27,7 +28,7 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findById(id).then(person => {
     if (person) {
@@ -37,20 +38,22 @@ app.get('/api/persons/:id', (request, response) => {
     }
   })
   .catch(error => {
-    console.error(error)
-    response.status(400).send({ error: 'malformatted id' })
+    next(error)
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndDelete(id).then(() => {
-  response.status(204).end()
-})
+    response.status(204).end()
+  })
+  .catch(error => {
+    next(error)
+  })
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -80,8 +83,54 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+    .catch(error => {
+        next(error)
+    })
 })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const { name, number } = request.body
+    if (!number) {
+      return response.status(400).json({ 
+        error: 'number missing' 
+      })
+    }
+    Person.findById(id).then(result => {
+      if (!result) {
+        return response.status(404).json({
+            error: 'person not found'
+        })
+        }
+        result.number = number
+        return result.save().then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+    })
+    .catch(error => {
+        next(error)
+    }
+)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
